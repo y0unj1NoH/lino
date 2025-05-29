14597c0146609fd6bfde23eb704e3b26838da29d
y0unj1NoH <nohyoong711@gmail.com>
2025-05-30 03:01:29 +0900

    ✨ ThemeToggle 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 
+import { ThemeToggle } from '@/shared/ui/ThemeToggle'
+
 export default function Page() {
   return (
     <div>
       <h1>Home</h1>
+      <ThemeToggle />
       <Link href="/about">About</Link>
     </div>
   )
