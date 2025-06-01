4932c0f11cffcdaa2daac0ec4ecc43a3b3647d4f
y0unj1NoH <nohyoong711@gmail.com>
2025-06-02 05:32:11 +0900

    :label: Task 관련 타입 추가

---
+++ src/entities/task/model/types.ts
@@ -0,0 +1,43 @@
+export enum TaskStatus {
+  Unassigned = 'unassigned',
+  UrgentImportant = 'urgent-important',
+  UrgentNotImportant = 'urgent-not-important',
+  NotUrgentImportant = 'not-urgent-important',
+  NotUrgentNotImportant = 'not-urgent-not-important',
+}
+
+export interface Task {
+  // UUID
+  id: string
+  // 할 일 내용
+  content: string
+  status: TaskStatus
+  // "오늘 바로 해야하는 일" 체크시 true
+  isToday: boolean
+  // 미루기 횟수 (기본값: 0)
+  postponedCount: number
+  // 수정 시각 (확장성/동기화 대비)
+  updatedAt: Date
+  // 완료 시각 (isCompleted 대신 사용)
+  completedAt?: Date
+}
+
+export type SortingStatus = 'UNSORTED' | 'SORTING' | 'SORTED'
+
+interface TaskState {
+  tasks: Task[]
+  sortingStatus: SortingStatus
+}
+interface TaskAction {
+  addTask: (content: string, isToday?: boolean) => void
+  updateTask: (taskId: string, content: string) => void
+  deleteTask: (taskId: string) => void
+  sortTask: (taskId: string, status: TaskStatus) => void
+  postponeTask: (taskId: string) => void
+  completeTask: (taskId: string) => void
+  uncompleteTask: (taskId: string) => void
+  setSortingStatus: (status: SortingStatus) => void
+  resetUnfinishedTasks: () => void
+}
+
+export type TaskStore = TaskState & TaskAction
