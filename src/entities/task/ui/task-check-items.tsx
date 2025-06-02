1b445d379b3db7d3c8b6839334fe57176c8e7903
y0unj1NoH <nohyoong711@gmail.com>
2025-06-03 01:33:31 +0900

    :sparkles: TaskCheckItems 구현

---
+++ src/entities/task/ui/task-check-items.tsx
@@ -0,0 +1,35 @@
+import { useTodaySortedTasks } from '@/entities/task/model/selector'
+import { useTaskStore } from '@/entities/task/model/slice'
+import { TaskStatus } from '@/entities/task/model/types'
+import { CheckboxWithLabel } from '@/shared/ui/checkbox-with-label'
+
+interface TaskCheckItemsProps {
+  status: TaskStatus
+}
+
+export const TaskCheckItems = ({ status }: TaskCheckItemsProps) => {
+  const tasks = useTodaySortedTasks(status)
+  const completeTask = useTaskStore((state) => state.completeTask)
+  const uncompleteTask = useTaskStore((state) => state.uncompleteTask)
+
+  return (
+    <div className="flex flex-col gap-2">
+      {tasks.map((task) => (
+        <CheckboxWithLabel
+          key={task.id}
+          id={task.id}
+          label={task.content}
+          checked={!!task.completedAt}
+          onCheckedChange={() => {
+            if (task.completedAt) {
+              uncompleteTask(task.id)
+            } else {
+              completeTask(task.id)
+            }
+          }}
+          Strikethrough
+        />
+      ))}
+    </div>
+  )
+}
