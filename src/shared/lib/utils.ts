393903400c23061d40fc4750a4f69ac2074f5e33
y0unj1NoH <nohyoong711@gmail.com>
2025-06-04 08:36:45 +0900

    :sparkles: once 추가

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function once(fn: () => void) {
+  let called = false
+  return () => {
+    if (!called) {
+      called = true
+      fn()
+    }
+  }
+}
