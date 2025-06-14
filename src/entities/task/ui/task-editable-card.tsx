bbca187ab731166354357fc444bcf691b87a050b
y0unj1NoH <nohyoong711@gmail.com>
2025-06-14 20:37:14 +0900

    :sparkles: TaskEditableList 구현

--- src/entities/task/ui/task-editable-card.tsx
+++ src/entities/task/ui/task-editable-card.tsx
@@ -19,7 +19,8 @@ export const TaskEditableCard = ({
   return (
     <Button
       variant="outline"
-      size="card"
+      // size="card"
+      className="w-full flex items-center justify-between gap-2 p-2 rounded-md border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
       onClick={onEdit}
       aria-label={`${content} 수정`}
     >
