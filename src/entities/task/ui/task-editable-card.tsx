478996fcb0240624c96aad1fc810d74a8d4f7f65
y0unj1NoH <nohyoong711@gmail.com>
2025-06-18 22:39:15 +0900

    :lipstick: overflow 시 줄바꿈 적용

--- src/entities/task/ui/task-editable-card.tsx
+++ src/entities/task/ui/task-editable-card.tsx
@@ -20,13 +20,11 @@ export const TaskEditableCard = ({
     <Button
       variant="outline"
       // size="card"
-      className="w-full flex items-center justify-between gap-2 p-2 rounded-md border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+      className="w-full h-auto flex items-center justify-between gap-2 p-2 rounded-md border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
       onClick={onEdit}
       aria-label={`${content} 수정`}
     >
-      <span className="flex-1 break-words whitespace-pre-wrap text-left">
-        {content}
-      </span>
+      <span className="flex-1 min-w-0 break-words text-left">{content}</span>
       <ButtonGhost
         size="iconSm"
         destructive
